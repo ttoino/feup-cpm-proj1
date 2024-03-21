ba603acc4b4665f413ecf6c8af92a0f5ec475eed
toino <joaoapereira21@hotmail.com>
2024-03-21 14:38:02 +0000

    Move out AuthLayout

--- feature/auth/src/main/kotlin/pt/up/fe/cpm/tiktek/feature/auth/LoginScreen.kt
+++ feature/auth/src/main/kotlin/pt/up/fe/cpm/tiktek/feature/auth/LoginScreen.kt
@@ -1,32 +1,20 @@
 package pt.up.fe.cpm.tiktek.feature.auth
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Key
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.parameters.CodeGenVisibility
 import com.ramcosta.composedestinations.generated.auth.destinations.AuthRouteDestination
@@ -34,6 +22,7 @@ import com.ramcosta.composedestinations.generated.auth.destinations.RegisterStar
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.popUpTo
 import pt.up.fe.cpm.tiktek.feature.auth.navigation.AuthGraph
+import pt.up.fe.cpm.tiktek.feature.auth.ui.AuthLayout
 
 @Destination<AuthGraph>(
     visibility = CodeGenVisibility.INTERNAL,
@@ -55,82 +44,46 @@ internal fun LoginScreen(
     onLogin: () -> Unit,
     onRegister: () -> Unit,
 ) {
-    Scaffold {
-        Column(
-            modifier = Modifier
-                .padding(it)
-                .padding(24.dp)
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-            ) {
-                Column {
-                    Text(
-                        stringResource(R.string.login_title),
-                        style = MaterialTheme.typography.headlineSmall
-                    )
-                    Text(
-                        stringResource(R.string.login_subtitle),
-                        style = MaterialTheme.typography.titleMedium,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                    )
-                }
+    AuthLayout(
+        title = R.string.login_title,
+        subtitle = R.string.login_subtitle,
+        mainAction = R.string.login_action,
+        onMainAction = onLogin,
+        secondaryAction = R.string.login_register_action,
+        onSecondaryAction = onRegister
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = {
+                Text(stringResource(R.string.email))
+            },
+            leadingIcon = {
+                Icon(Icons.Default.Email, contentDescription = stringResource(R.string.email))
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
 
-                Spacer(modifier = Modifier.height(32.dp))
-
-                TextField(
-                    value = "",
-                    onValueChange = {},
-                    label = {
-                        Text(stringResource(R.string.email))
-                    },
-                    leadingIcon = {
-                        Icon(Icons.Default.Email, contentDescription = stringResource(R.string.email))
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Email,
-                        imeAction = ImeAction.Next
-                    ),
-                    modifier = Modifier.fillMaxWidth()
-                )
-
-                TextField(
-                    value = "",
-                    onValueChange = {},
-                    label = {
-                        Text(stringResource(R.string.password))
-                    },
-                    leadingIcon = {
-                        Icon(Icons.Default.Key, contentDescription = stringResource(R.string.password))
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Password,
-                        imeAction = ImeAction.Done
-                    ),
-                    visualTransformation = PasswordVisualTransformation(),
-                    modifier = Modifier.fillMaxWidth()
-                )
-            }
-
-            Column {
-                Button(
-                    onClick = { onLogin() },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(stringResource(R.string.login_action))
-                }
-
-                TextButton(
-                    onClick = { onRegister() },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(stringResource(R.string.login_register_action))
-                }
-            }
-        }
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = {
+                Text(stringResource(R.string.password))
+            },
+            leadingIcon = {
+                Icon(Icons.Default.Key, contentDescription = stringResource(R.string.password))
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
--- feature/auth/src/main/kotlin/pt/up/fe/cpm/tiktek/feature/auth/RegisterScreen.kt
+++ feature/auth/src/main/kotlin/pt/up/fe/cpm/tiktek/feature/auth/RegisterScreen.kt
@@ -1,13 +1,8 @@
 package pt.up.fe.cpm.tiktek.feature.auth
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CreditCard
@@ -18,19 +13,12 @@ import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Pin
 import androidx.compose.material.icons.filled.Today
-import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -47,6 +35,7 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.popUpTo
 import pt.up.fe.cpm.tiktek.core.ui.form.SeparatorVisualTransformation
 import pt.up.fe.cpm.tiktek.feature.auth.navigation.AuthGraph
+import pt.up.fe.cpm.tiktek.feature.auth.ui.AuthLayout
 
 @Destination<AuthGraph>(
     visibility = CodeGenVisibility.INTERNAL,
@@ -83,134 +72,98 @@ internal fun RegisterStartScreen(
     onContinue: () -> Unit,
     onLogin: () -> Unit,
 ) {
-    Scaffold {
-        Column(
-            modifier = Modifier
-                .padding(it)
-                .padding(24.dp)
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
+    AuthLayout(
+        title = R.string.register_title,
+        subtitle = R.string.register_start_subtitle,
+        mainAction = R.string.register_continue_action,
+        onMainAction = onContinue,
+        secondaryAction = R.string.register_login_action,
+        onSecondaryAction = onLogin
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = {
+                Text(stringResource(R.string.name))
+            },
+            leadingIcon = {
+                Icon(Icons.Default.Person, contentDescription = stringResource(R.string.name))
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-            ) {
-                Column {
-                    Text(
-                        stringResource(R.string.register_title),
-                        style = MaterialTheme.typography.headlineSmall
-                    )
-                    Text(
-                        stringResource(R.string.register_start_subtitle),
-                        style = MaterialTheme.typography.titleMedium,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                    )
-                }
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text(stringResource(R.string.nif))
+                },
+                leadingIcon = {
+                    Icon(Icons.Default.Pin, contentDescription = stringResource(R.string.nif))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.weight(1f)
+            )
 
-                Spacer(modifier = Modifier.height(32.dp))
-
-                TextField(
-                    value = "",
-                    onValueChange = {},
-                    label = {
-                        Text(stringResource(R.string.name))
-                    },
-                    leadingIcon = {
-                        Icon(Icons.Default.Person, contentDescription = stringResource(R.string.name))
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        imeAction = ImeAction.Next
-                    ),
-                    modifier = Modifier.fillMaxWidth()
-                )
-
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    TextField(
-                        value = "",
-                        onValueChange = {},
-                        label = {
-                            Text(stringResource(R.string.nif))
-                        },
-                        leadingIcon = {
-                            Icon(Icons.Default.Pin, contentDescription = stringResource(R.string.nif))
-                        },
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Number,
-                            imeAction = ImeAction.Next
-                        ),
-                        modifier = Modifier.weight(1f)
-                    )
-
-                    TextField(
-                        value = "",
-                        onValueChange = {},
-                        label = {
-                            Text(stringResource(R.string.birthdate))
-                        },
-                        leadingIcon = {
-                            Icon(Icons.Default.Today, contentDescription = stringResource(R.string.birthdate))
-                        },
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Number,
-                            imeAction = ImeAction.Next
-                        ),
-                        modifier = Modifier.weight(1f)
-                    )
-                }
-
-                TextField(
-                    value = "",
-                    onValueChange = {},
-                    label = {
-                        Text(stringResource(R.string.email))
-                    },
-                    leadingIcon = {
-                        Icon(Icons.Default.Email, contentDescription = stringResource(R.string.email))
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Email,
-                        imeAction = ImeAction.Next
-                    ),
-                    modifier = Modifier.fillMaxWidth()
-                )
-
-                TextField(
-                    value = "",
-                    onValueChange = {},
-                    label = {
-                        Text(stringResource(R.string.password))
-                    },
-                    leadingIcon = {
-                        Icon(Icons.Default.Key, contentDescription = stringResource(R.string.password))
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Password,
-                        imeAction = ImeAction.Done
-                    ),
-                    visualTransformation = PasswordVisualTransformation(),
-                    modifier = Modifier.fillMaxWidth()
-                )
-            }
-
-            Column {
-                Button(
-                    onClick = { onContinue() },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(stringResource(R.string.register_continue_action))
-                }
-
-                TextButton(
-                    onClick = { onLogin() },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(stringResource(R.string.register_login_action))
-                }
-            }
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text(stringResource(R.string.birthdate))
+                },
+                leadingIcon = {
+                    Icon(Icons.Default.Today, contentDescription = stringResource(R.string.birthdate))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                modifier = Modifier.weight(1f)
+            )
         }
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = {
+                Text(stringResource(R.string.email))
+            },
+            leadingIcon = {
+                Icon(Icons.Default.Email, contentDescription = stringResource(R.string.email))
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = {
+                Text(stringResource(R.string.password))
+            },
+            leadingIcon = {
+                Icon(Icons.Default.Key, contentDescription = stringResource(R.string.password))
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
@@ -219,137 +172,101 @@ internal fun RegisterFinishScreen(
     onRegister: () -> Unit,
     onLogin: () -> Unit,
 ) {
-    Scaffold {
-        Column(
-            modifier = Modifier
-                .padding(it)
-                .padding(24.dp)
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
+    AuthLayout(
+        title = R.string.register_title,
+        subtitle = R.string.register_finish_subtitle,
+        mainAction = R.string.register_action,
+        onMainAction = onRegister,
+        secondaryAction = R.string.register_login_action,
+        onSecondaryAction = onLogin
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = {
+                Text(stringResource(R.string.name_cc))
+            },
+            leadingIcon = {
+                Icon(Icons.Default.Person, contentDescription = stringResource(R.string.name_cc))
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        TextField(
+            value = "",
+            onValueChange = { },
+            label = {
+                Text(stringResource(R.string.number_cc))
+            },
+            leadingIcon = {
+                Icon(Icons.Default.CreditCard, contentDescription = stringResource(R.string.number_cc))
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            visualTransformation = SeparatorVisualTransformation(4, maxSize = 16),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-            ) {
-                Column {
-                    Text(
-                        stringResource(R.string.register_title),
-                        style = MaterialTheme.typography.headlineSmall
-                    )
-                    Text(
-                        stringResource(R.string.register_finish_subtitle),
-                        style = MaterialTheme.typography.titleMedium,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                    )
-                }
 
-                Spacer(modifier = Modifier.height(32.dp))
+            TextField(
+                value = "",
+                onValueChange = { },
+                label = {
+                    Text(stringResource(R.string.expiry_cc))
+                },
+                leadingIcon = {
+                    Icon(Icons.Default.DateRange, contentDescription = stringResource(R.string.expiry_cc))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Next
+                ),
+                visualTransformation = SeparatorVisualTransformation(2, '/', 4),
+                modifier = Modifier.weight(1f)
+            )
 
-                TextField(
-                    value = "",
-                    onValueChange = {},
-                    label = {
-                        Text(stringResource(R.string.name_cc))
-                    },
-                    leadingIcon = {
-                        Icon(Icons.Default.Person, contentDescription = stringResource(R.string.name_cc))
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        imeAction = ImeAction.Next
-                    ),
-                    modifier = Modifier.fillMaxWidth()
-                )
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = {
+                    Text(stringResource(R.string.cvc_cc))
+                },
+                leadingIcon = {
+                    Icon(Icons.Default.Lock, contentDescription = stringResource(R.string.cvc_cc))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                modifier = Modifier.weight(1f)
+            )
+        }
 
-                TextField(
-                    value = "",
-                    onValueChange = { },
-                    label = {
-                        Text(stringResource(R.string.number_cc))
-                    },
-                    leadingIcon = {
-                        Icon(Icons.Default.CreditCard, contentDescription = stringResource(R.string.number_cc))
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Number,
-                        imeAction = ImeAction.Next
-                    ),
-                    visualTransformation = SeparatorVisualTransformation(4, maxSize = 16),
-                    modifier = Modifier.fillMaxWidth()
-                )
+        Text(
+            stringResource(R.string.data_collection_advisory),
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
 
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                    modifier = Modifier.fillMaxWidth()
-                ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+        ) {
+            Checkbox(
+                checked = false,
+                onCheckedChange = { }
+            )
 
-                    TextField(
-                        value = "",
-                        onValueChange = { },
-                        label = {
-                            Text(stringResource(R.string.expiry_cc))
-                        },
-                        leadingIcon = {
-                            Icon(Icons.Default.DateRange, contentDescription = stringResource(R.string.expiry_cc))
-                        },
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Number,
-                            imeAction = ImeAction.Next
-                        ),
-                        visualTransformation = SeparatorVisualTransformation(2, '/', 4),
-                        modifier = Modifier.weight(1f)
-                    )
-
-                    TextField(
-                        value = "",
-                        onValueChange = {},
-                        label = {
-                            Text(stringResource(R.string.cvc_cc))
-                        },
-                        leadingIcon = {
-                            Icon(Icons.Default.Lock, contentDescription = stringResource(R.string.cvc_cc))
-                        },
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Number,
-                            imeAction = ImeAction.Done
-                        ),
-                        modifier = Modifier.weight(1f)
-                    )
-                }
-
-                Text(
-                    stringResource(R.string.data_collection_advisory),
-                    style = MaterialTheme.typography.labelSmall,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                )
-
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(4.dp),
-                ) {
-                    Checkbox(
-                        checked = false,
-                        onCheckedChange = { }
-                    )
-
-                    Text(stringResource(R.string.terms_and_conditions))
-                }
-            }
-
-            Column {
-                Button(
-                    onClick = { onRegister() },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(stringResource(R.string.register_action))
-                }
-
-                TextButton(
-                    onClick = { onLogin() },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(stringResource(R.string.register_login_action))
-                }
-            }
+            Text(stringResource(R.string.terms_and_conditions))
         }
     }
 }
