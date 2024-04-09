dadb3f59ee1d2d5047a3aa4c37170c1014057b18
Anete <anetempereira@gmail.com>
2024-04-09 23:18:45 +0100

    [Cafeteria Terminal] Updated page according to whats asked in project specification and made data come from json object

--- app/cafeteria/src/main/kotlin/pt/up/fe/cpm/tiktek/cafeteria/MainActivity.kt
+++ app/cafeteria/src/main/kotlin/pt/up/fe/cpm/tiktek/cafeteria/MainActivity.kt
@@ -2,10 +2,36 @@ package pt.up.fe.cpm.tiktek.cafeteria
 
 import androidx.compose.runtime.Composable
 import dagger.hilt.android.AndroidEntryPoint
+import org.json.JSONObject
 import pt.up.fe.cpm.tiktek.core.app.ScreenActivity
 
 @AndroidEntryPoint
 class MainActivity : ScreenActivity() {
+    val jsonString = """
+    {
+      "orderNumber": 123456,
+      "orderProducts": [
+        {
+          "cafetariaItem": "Coffee",
+          "quantity": 2,
+          "totalPrice": 200
+        },
+        {
+          "cafetariaItem": "Croissant",
+          "quantity": 1,
+          "totalPrice": 50
+        }
+      ],
+      "orderVouchers": [
+        {
+          "voucherName": "1 café grátis",
+          "voucherQuantity": 1
+        }
+      ]
+    }
+    """
+    val purchase = JSONObject(jsonString)
+
     @Composable
-    override fun Screen() = PurchasedProductsScreen()
+    override fun Screen() = PurchasedProductsScreen(purchase = purchase)
 }
--- app/cafeteria/src/main/kotlin/pt/up/fe/cpm/tiktek/cafeteria/PurchasedProductsScreen.kt
+++ app/cafeteria/src/main/kotlin/pt/up/fe/cpm/tiktek/cafeteria/PurchasedProductsScreen.kt
@@ -6,11 +6,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -38,12 +40,15 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import org.json.JSONObject
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PurchasedProductsScreen() {
+fun PurchasedProductsScreen(purchase: JSONObject) {
     var scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
-
+    val orderNumber = purchase.getInt("orderNumber")
+    val orderProducts = purchase.getJSONArray("orderProducts")
+    val orderVouchers = purchase.getJSONArray("orderVouchers")
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
@@ -57,38 +62,60 @@ fun PurchasedProductsScreen() {
             )
         },
     ) {
-        Column(
-            modifier =
-            Modifier
-                .padding(it)
-                .verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally
+        LazyColumn(
+            modifier = Modifier.padding(it).fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            PurchasedProduct(
-                cafetariaItem = "café",
-                quantity = 2,
-                totalPrice = 2,
-            )
-            PurchasedProduct(
-                cafetariaItem = "pipoca",
-                quantity = 1,
-                totalPrice = 4
-            )
-            Spacer(
-                modifier = Modifier.height(50.dp),
-            )
-            Button(
-                onClick = { /*TODO*/ },
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.TaskAlt,
-                    contentDescription = "Validar bilhete",
-                    modifier = Modifier.size(18.dp),
+            item {
+                Text(
+                    text = "Número de pedido: $orderNumber",
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
                 )
-                Spacer(
-                    modifier = Modifier.width(8.dp),
+            }
+
+            items(orderProducts.length()) { index ->
+                val product = orderProducts.getJSONObject(index)
+                PurchasedProduct(
+                    cafetariaItem = product.getString("cafetariaItem"),
+                    quantity = product.getInt("quantity"),
+                    totalPrice = product.getInt("totalPrice"),
                 )
-                Text(text = "Recolher compra")
+            }
+
+            item {
+                Text(
+                    text = "Vouchers utilizados",
+                    fontSize = 25.sp,
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                )
+            }
+
+            items(orderVouchers.length()) { index ->
+                val voucher = orderVouchers.getJSONObject(index)
+                Voucher(
+                    voucherName = voucher.getString("voucherName"),
+                    voucherQuantity = voucher.getInt("voucherQuantity"),
+                )
+            }
+
+            item {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.TaskAlt,
+                        contentDescription = "Validar bilhete",
+                        modifier = Modifier.size(18.dp),
+                    )
+                    Spacer(
+                        modifier = Modifier.width(8.dp),
+                    )
+                    Text(text = "Recolher compra")
+                }
             }
         }
     }
@@ -132,6 +159,50 @@ private fun PurchasedProduct(
                 )
                 Text(
                     text = "$totalPrice.00€", // TODO: Change to a function that formats the money
+                    fontSize = 15.sp,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun Voucher(
+    voucherName: String,
+    voucherQuantity: Int,
+) {
+    Card(
+        border =
+        BorderStroke(
+            2.dp,
+            MaterialTheme.colorScheme.outlineVariant,
+        ),
+        colors =
+        CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        modifier =
+        Modifier
+            .padding(5.dp)
+            .clickable(
+                onClick = { },
+            ),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+            ) {
+                Text(
+                    text = voucherName,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+                Text(
+                    text = "Quantidade utilizada: $voucherQuantity", // TODO: Change to a function that formats the money
                     fontSize = 15.sp,
                 )
             }
