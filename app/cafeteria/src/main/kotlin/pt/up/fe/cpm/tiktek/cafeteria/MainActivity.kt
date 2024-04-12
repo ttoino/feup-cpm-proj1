e5b30aa3b0dd69ae7874069f724f69530bf87aad
Anete <anetempereira@gmail.com>
2024-04-12 17:34:50 +0100

    [Cafeteria Terminal] WIP: QR Code workingggg (still needs to test receiving data but still an achievementtt)

--- app/cafeteria/src/main/kotlin/pt/up/fe/cpm/tiktek/cafeteria/MainActivity.kt
+++ app/cafeteria/src/main/kotlin/pt/up/fe/cpm/tiktek/cafeteria/MainActivity.kt
@@ -1,6 +1,18 @@
 package pt.up.fe.cpm.tiktek.cafeteria
 
+import android.app.Instrumentation.ActivityResult
+import android.content.Context
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.core.content.ContextCompat
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 import dagger.hilt.android.AndroidEntryPoint
 import org.json.JSONObject
 import pt.up.fe.cpm.tiktek.core.app.ScreenActivity
@@ -32,6 +44,50 @@ class MainActivity : ScreenActivity() {
     """
     val purchase = JSONObject(jsonString)
 
+    var textResult = mutableStateOf("")
+
+    val barCodeLauncher = registerForActivityResult(ScanContract()){
+            result ->
+        if (result.contents == null){
+            Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_SHORT).show()
+        } else {
+            textResult.value = result.contents
+        }
+    }
+
+    fun showCamera() {
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan your cart QR code")
+        options.setCameraId(0)
+        options.setBeepEnabled(false)
+        options.setOrientationLocked(false)
+
+        barCodeLauncher.launch((options))
+
+    }
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        isGranted ->
+        if(isGranted){
+            showCamera()
+        }
+    }
+
+    private fun checkCameraPermission(context: Context){
+        if (ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED){
+            showCamera()
+        } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
+            Toast.makeText(this@MainActivity, "Camera required", Toast.LENGTH_SHORT).show()
+        } else {
+            requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+        }
+    }
     @Composable
-    override fun Screen() = PurchasedProductsScreen(purchase = purchase)
+    override fun Screen() = MainScreen(::checkCameraPermission)
+    //override fun Screen() = PurchasedProductsScreen(purchase = purchase)
 }
